834ac3272fc2b02fe90db7d51cff92b51d1ade3d
Chitra Goyal <chitragoyal@Chitras-MacBook-Pro-3.local>
2018-06-02 19:06:07 +0530

    ParkingLotTest.java

--- ParkingLotTest.java
+++ ParkingLotTest.java
@@ -12,50 +12,43 @@ public class ParkingLotTest {
 	private static final PrintStream os = null;
 	private static BufferedReader inputLine = null;
 	private static boolean closed = false;
-		
+	private static ParkingSlot PS = new ParkingSlot();
+	
 	public static void main(String args[]) {
-	    // need
+	    
+		
 	    if (args.length > 0) {
+	    	System.out.println(args[0]);
+			String fileName = args[0];        
+			String line = null;
+
+			try {
+				// FileReader reads text files in the default encoding.
+			    FileReader fileReader = new FileReader(fileName);
+
+			    // Always wrap FileReader in BufferedReader.
+			    BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			    while((line = bufferedReader.readLine()) != null) {
+			    	System.out.println(line);
+			    	handle_read_string(line);
+			    }   
+
+			    // Always close files.
+			    bufferedReader.close();         
+			}
+			catch(FileNotFoundException ex) {
+			      System.out.println("Unable to open file '" + fileName + "'");                
+			}
+			catch(IOException ex) {
+			      System.out.println("Error reading file '" + fileName + "'");                  
+			      // Or we could just do this: 
+			      // ex.printStackTrace();
+			}
 			    
-			    	System.out.println(args[0]);
-			    	 // The name of the file to open.
-			        String fileName = args[0];
-
-			        // This will reference one line at a time
-			        String line = null;
-
-			        try {
-			            // FileReader reads text files in the default encoding.
-			            FileReader fileReader = 
-			                new FileReader(fileName);
-
-			            // Always wrap FileReader in BufferedReader.
-			            BufferedReader bufferedReader = 
-			                new BufferedReader(fileReader);
-
-			            while((line = bufferedReader.readLine()) != null) {
-			                System.out.println(line);
-			            }   
-
-			            // Always close files.
-			            bufferedReader.close();         
-			        }
-			        catch(FileNotFoundException ex) {
-			            System.out.println(
-			                "Unable to open file '" + 
-			                fileName + "'");                
-			        }
-			        catch(IOException ex) {
-			            System.out.println(
-			                "Error reading file '" 
-			                + fileName + "'");                  
-			            // Or we could just do this: 
-			            // ex.printStackTrace();
-			        }
-			    
-			  }
+		}
 		 
-		ParkingSlot PS = new ParkingSlot();// eclipse is givng an error
+		
 		inputLine = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			while (!closed) {
@@ -63,12 +56,22 @@ public class ParkingLotTest {
 				String str =  inputLine.readLine().trim();
 				System.out.println(str);
 				str.trim();
-			if (str.startsWith("create_parking_lot")) {
+				handle_read_string(str);
+			
+			} 
+		} catch (IOException e) {
+				 
+		}
+	}
+
+	public static void handle_read_string(String str) {
+
+		if (str.startsWith("create_parking_lot")) {
 				String[] tokens = str.split(" ");
 				int number = Integer.parseInt(tokens[1]);	
 				PS = new ParkingSlot(number);
 			}
-			
+		else
 			if (str.startsWith("park")) {
 				String[] tokens = str.split(" ");
 				String carNumber = tokens[1];
@@ -78,39 +81,35 @@ public class ParkingLotTest {
 				//slot number to be printed is 1 more than the index returned
 				System.out.println("Allocated slot number: " + slotNumber);
 			}
-			if (str.startsWith("leave")) {
-				String[] tokens = str.split(" ");
-				int slotNumber = Integer.parseInt(tokens[1]);	
-				//leave <slotNumber>
-				//error checks have to be put in
-				PS.removeVehicle(slotNumber);
-				System.out.println("Slot number " + slotNumber + " is free");
+			else if (str.startsWith("leave")) {
+					String[] tokens = str.split(" ");
+					int slotNumber = Integer.parseInt(tokens[1]);	
+					//leave <slotNumber>
+					//error checks have to be put in
+					PS.removeVehicle(slotNumber);
+					System.out.println("Slot number " + slotNumber + " is free");
 			
-			}
-			if (str.startsWith("status")) {
-				
+			} else if (str.startsWith("status")) {
 				PS.getParkingStatus();
-				
-			
 			}
-			if (str.startsWith("registration_numbers_for_cars_with_color")) {
+			else if (str.startsWith("registration_numbers_for_cars_with_color")) {
 				
 				String[] tokens = str.split(" ");
-				String carNumber = tokens[1];
-				
+				String carColor = tokens[1];
+				String retString = PS.handle_registration_number_for_cars_with_color(carColor);
 			
 			}
-			if (str.startsWith("registration_numbers_for_cars_with_color")) {
+			else if (str.startsWith("slot_number_for_cars_with_colour")) {
 				
 				String[] tokens = str.split(" ");
-				String carNumber = tokens[1];
+				String carColor = tokens[1];
+				String retStr = PS.handle_slot_number_for_cars_with_color(carColor);
 				
 			
+			} else if (str.startsWith("slot_number_for_registration_number")) {
+				String[] tokens = str.split(" ");
+				String carReg = tokens[1];
+				String retStr = PS.handle_slot_number_for_registration_number(carReg);
 			}
-			
-		} 
-	} catch (IOException e) {
-				 
 	}
-}
 }
